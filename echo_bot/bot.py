9bb928af832f66642002ab23e577eea878b41a99
Natasha <znatashka@ya.ru>
2016-11-03 10:35:07 +0300

    add echo bot

---
+++ echo_bot/bot.py
@@ -0,0 +1,14 @@
+import telebot
+
+from echo_bot import config
+
+bot = telebot.TeleBot(config.token)
+
+
+@bot.message_handler(content_types=['text'])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
